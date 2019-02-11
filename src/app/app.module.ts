9cabaa53bce7b46f42504e22f6023dcb6c84a303
scorpio611 <evilmonter666@gmail.com>
2019-02-11 17:48:00 +0700

    [Thực hành] Hiển thị thông tin thú nuôi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import {CalculatorComponent} from './calculator/calculator.component';
 import {ProfileComponent} from './profile/profile.component';
 import {FormsModule} from '@angular/forms';
 import {FontSizeEditorComponent} from './font-size-editor/font-size-editor.component';
+import {PetComponent} from './pet/pet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
     ProfileComponent,
-    FontSizeEditorComponent
+    FontSizeEditorComponent,
+    PetComponent
   ],
   imports: [
     BrowserModule,
